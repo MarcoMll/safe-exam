a2ee7606bbf0f628baa257b595e1bae52d2f9dcb
Marco <marcomalliani@gmail.com>
2026-07-06 20:23:52 +0300

    Update object_detector.py

--- src/object_detector.py
+++ src/object_detector.py
@@ -1,16 +1,16 @@
 from pathlib import Path
 from ultralytics import YOLO
-from utils.paths_initializer import get_paths
+from src.safe_exam.utils.paths_initializer import get_paths
 
 project_paths = get_paths()
-path_to_test_image: Path = project_paths.RAW_DIR / "student_with_phone_test_3.png"
+path_to_test_image: Path = project_paths.RAW_DIR / "student_with_phone_test.png"
 model_path = project_paths.MODELS_DIR / "yolo26s.pt"
 
 model = YOLO(model_path)
 
 results = model.predict(
     source=path_to_test_image,
-    classes=[67],
+    classes=[0, 67],
 )
 
 for r in results:
